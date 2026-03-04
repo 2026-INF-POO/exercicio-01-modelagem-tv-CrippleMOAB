809dcbcf8da005d3d2ff974b4e84cf1c7839e638
CrippleMOAB <moisesmanoel645@gmail.com>
2026-03-04 17:50:43 -0300

    Update TV.java

--- TV.java
+++ TV.java
@@ -1,1 +1,48 @@
-
+class Tv{
+  double polegadas;
+  int volume=5;
+  String marca;
+  int voltagem;
+  int canal=1;
+  int ligar(int voltagem, int polegadas){
+    return voltagem*polegadas;
+  }
+  void desligar(){
+    System.out.println("Desligando...");
+  }
+  int aumentarVolume(int volume){
+    if(volume==10){
+      System.out.println("Volume máximo atingido!");
+    }
+    else{
+      volume++;
+      System.out.println("Volume: "+volume);
+    }
+    return volume;
+  }
+  int diminuirVolume(int volume){
+    if(volume==1){
+      System.out.println("Volume mínimo atingido!");
+    }
+    else{
+      volume--;
+      System.out.println("Volume: "+volume);
+    }
+    return volume;
+  }
+  int subirCanal(int canal){
+    canal++;
+    System.out.println("Canal: "+canal);
+    return canal;
+  }
+  int descerCanal(int canal){
+    if(canal>1){
+      canal--;
+      System.out.println("Canal: "+canal);
+    }
+    else{
+      System.out.println("Canal inválido!");
+    }
+    return canal;
+  }
+}
